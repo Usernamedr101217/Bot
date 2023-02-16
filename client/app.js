27d1622c18d01cbc40a11e2ec781779662e517b9
Usernamedr101217 <123254102+Usernamedr101217@users.noreply.github.com>
2023-02-16 07:31:10 +0700

    Add files via upload

--- client/app.js
+++ client/app.js
@@ -84,7 +84,7 @@ async function handleSubmit(e) {
     const message = document.getElementById(id);
     loading(message);  
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://botserver2.onrender.com', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
